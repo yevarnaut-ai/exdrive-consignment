ed2915cf4530a22384cd2313a4ab2bba102bdf77
yevarnaut-ai <yevarnaut@gmail.com>
2026-09-11 12:19:43 -0700

    Update Formspree URL in fetch request

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ export default function ExdriveConsignment() {
     setError('');
 
     try {
-      const response = await fetch('https://formspree.io/f/YOUR_FORM_ID', {
+      const response = await fetch('https://formspree.io/f/xoeqbrnn', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
